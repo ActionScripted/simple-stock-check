9c725d68245be4ee974fb4543ec614264cf9408f
Michael Thompson <michael@traversemagazine.com>
2015-06-19 21:22:21 -0400

    adding stub

---
+++ simple_lookup.py
@@ -0,0 +1,14 @@
+from yahoo_finance import Share
+
+
+PORTFOLIO = {
+    'HILL': 7.14,
+    'BRCD': 12.35,
+}
+
+
+def investment_check(portfolio):
+    pass
+
+if __name__ == '__main__':
+    investment_check(PORTFOLIO)
